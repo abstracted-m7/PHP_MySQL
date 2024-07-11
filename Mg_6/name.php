c888acc937d7d636659afc87612aacf2e9be08d5
Manish Giri <142075284+abstracted-m7@users.noreply.github.com>
2024-07-11 20:27:57 +0530

    Add files via upload

--- Mg_6/name.php
+++ Mg_6/name.php
@@ -11,126 +11,205 @@
 <div class="phpcontainer">
 <?php
 echo "<pre>";
+// --------------------Assignment- 1 & 2-----------------
 echo "<h1>Welcome</h1>";
 echo "<h2>Mr.$_POST[fname] $_POST[lname]</h2>";
-echo "<h2>$_POST[p_number]</h2>";
+// --------------------Assignment-3-----------------
+echo "<h2>Phone Number :$_POST[p_number]</h2>";
 
-// -------------------Check Even Odd-------------------------
+// ------------------- asst-4 Check Even Odd-------------------------
 
-echo "*****************************";
-echo "<br>Check Even Odd :";
+echo "<br>************************************";
+echo "<br>Check Even Odd";
 $num=$_POST['EO_number'];
 if ($num%2==0) {
-  echo "<h4>The Number is : <b>Even</b></h4>";
+  echo "The Number is : Even";
 }
 else{
-    echo "<h4>The Number is : <b>Odd</b></h4>";
+    echo "The Number is : Odd";
 }
-
-// -------------------Check Number greater or smaller------------------------- 
+echo "<br>************************************";
+echo "<br>";
+// ------------------- asst-5 Check Number greater or smaller-------------------------
 echo "<br>************************************";
 echo "<br>Find Number is greater or smaller ?";
 $num=$_POST['first_number'];
 $num2=$_POST['second_number'];
 $num3=$_POST['third_number'];
 echo "<br>The numbers are :".$num." , ".$num2." and ".$num3;
-if ($num>$num2 and $num>$num3) {
+if ($num>$num2 and $num > $num3) {
   echo "<br>The greater number is : ".$num;
 }
-elseif($num2>$num and $num2>$num3){
-  echo "<br>The greater number is : ".$num2;
+elseif ($num2>$num and $num2 > $num3) {
+    echo "<br>The greater number is : ".$num2;
 }
 else{
-  echo "<br>The greater number is : ".$num3;
+   echo "<br>The greater number is : ".$num3;
 }
 echo "<br>************************************";
-
-// Using tarnary operator to find bigger number
-
-echo "<br>************************************";
-echo "<br>Using tarnary operator find bigger number ?<br>";
-
-$num=$_POST['first_number'];
-$num2=$_POST['second_number'];
-
-echo "<br>The num1 is : ".$num;
-echo "<br>The num2 is : ".$num2;
 echo "<br>";
-echo ($num>$num2) ? "Num1 is bigger" : "Num2 is bigger";
+// ------------------- asst-6 Swap two numbers-------------------------
 echo "<br>************************************";
-// Using only nested if-else find the bigger number
+echo "<br>Swapping the two Numbers : ";
+$x = $_POST["swapNum1"];
+$y = $_POST["swapNum2"];
+echo "<br>Before swapping: x= $x , y= $y ";
+echo "<br />";
+$z = $x;
+$x = $y;
+$y = $z;
+echo "After swapping: x= $x , y= $y ";
 echo "<br>************************************";
-echo "<br>Using only nested if-else find the bigger number";
-
-$num=$_POST['first_number'];
-$num2=$_POST['second_number'];
-$num3=$_POST['third_number'];
-
-echo "<br>The numbers is : $num , $num2 , $num3";
-if ($num>$num2) {
-  if ($num>$num3) {
-    echo "<br>The bigger number is : $num";
-  }
-  else{
-    echo "<br>The bigger number is : $num3";
-  }
+echo "<br>";
+// ------------------- asst-7 Multiplication Table-------------------------
+echo "<br>************************************";
+echo "<br>Multiplication Table for Number :".$_POST['multiplyNumber']."<br>" ;
+for($i=1;$i<=10;$i++){
+	$result=$_POST['multiplyNumber']*$i;
+	echo $_POST['multiplyNumber']." * ".$i." = ". $result;
+	echo "<br>";
 }
-elseif($num2>$num3) {
-  echo "<br>The bigger number is : $num2";
-}
-else{
-  echo "<br>The bigger number is : $num3";
-}
-
 echo "<br>************************************";
+echo "<br>";
 
-// -------------------Check number prime or not------------------------- 
+// ------------------- asst-8 Mathematical calculator-------------------------
 echo "<br>************************************";
-echo "<br>Prime number find ?";
+echo "<br>Methematical Calculator : ";
+$operand1=$_POST['operand1'];
+$operator=$_POST['operator'];
+$operand2=$_POST['operand2'];
 
-$prime=$_POST['p_num'];
-echo "<br>The number is : ".$prime;
-if ($prime>1) {
-  for ($i=2; $i<=$prime/2; $i++) {
-    $flag=0;
-    if ($prime%$i==0) {
-      $flag=1;
+echo "<br>The operand1: $operand1 and operand2: $operand2";
+echo "<br>The operator is : $operator";
+
+switch ($operator){
+  case "+":
+     $result=$operand1+$operand2;
+    break;
+  case "-":
+     $result=$operand1-$operand2;
+    break;
+   case "*":    
+      $result=$operand1*$operand2;
+    break;
+    case "/":    
+        $result=$operand1/$operand2;
       break;
-    }
-  }
-  if ($flag==0) {
-    echo "<br>The number is : Prime";
-  }
-  else{
-    echo "<br>The number is : Not Prime";
-  }
+   default:
+     $result="Invalid Operator! Please use +, - or *.";
+}
+echo "<br>The result of the operation is:".$result;
+echo "<br>************************************";
+echo "<br>";
 
+// ------------------- asst-9 Display Current Date -------------------------
+echo "<br>************************************<br>";
+echo "Today's Date : ".date("Y-m-d"); // this will display current date in YYYY
+// MM (Month) & DD (Day)
+echo "<br>Current Month: ".date("F")."<br>Current Day:".date("d");
+echo "<br>Today's Weekday: ".date("l");
+echo "<br>Weekday Number: ".date("w"); 
+echo "<br>************************************";
+
+// ------------------- asst-10 Palindrome or not -------------------------
+echo "<br>************************************";
+echo "<br>Check a string is palindrome or not";
+$string = $_POST['string'];
+$stringLower = strtolower($string);
+if(strrev($stringLower) == $stringLower) {
+    echo "<br>$string is a palindrome.";
+} else {
+  echo "<br>$string is not a palindrome.";
 }
 echo "<br>************************************";
 
-// -------------------Swap variable-------------------------
+// check a number is palindrome or not
 echo "<br>************************************";
-$snum1=$_POST['s_num1'];
-$snum2=$_POST['s_num2'];
-echo "<br>Using temporary variable : ";
-echo "<br>Before swaping numbers is : $snum1, $snum2";
-$temp=$snum1; 
-$snum1=$snum2;
-$snum2=$temp;
-echo "<br>After swaping numbers is : $snum1, $snum2";
+echo "<br>Check a number is palindrome or not";
+$number=$_POST['number'];
+if(is_numeric($number)) {
+  if(strrev($number)==$number){
+    echo "<br>This number is a palindrome.";
+    }else{
+       echo "<br>This number is not a palindrome.";
+    }
+}else{
+  echo "<br>Please enter numeric input only.";
+}
+echo "<br>************************************";
 
-// using addition and substraction create swap
-echo "<br><br>Does not Using temporary variable : ";
-$swapnum1=$_POST['s_num1'];
-$swapnum2=$_POST['s_num2'];
-echo "<br>Before swaping numbers is : $swapnum1, $swapnum2";
 
-$swapnum1=$swapnum1+$swapnum2;
-$swapnum2=$swapnum1-$swapnum2;
-$swapnum1=$swapnum1-$swapnum2;
-echo "<br>After swaping numbers is : $swapnum1, $swapnum2";
+// ------------------- asst-11 Armstrong or not -------------------------
+echo "<br>************************************";
+echo "<br>Check whether the given number is an Armstrong number or not?<br>";
+
+// formula : abcd... = an + bn + cn + dn + 
+$num = $_POST['armnstrg'];
+$originalNum = $num; // Store the original value
+$sum = 0;
+$count = 0;
+
+while ($num != 0) {
+   $rem = $num % 10;
+   $sum += $rem * $rem * $rem;
+   $num /= 10;
+}
+
+if ($sum == $originalNum) // Compare with the original value
+   echo "<br>The number <b>".$originalNum."</b> is an Armstrong number.";
+else
+   echo "<br>The number <b>".$originalNum."</b> is not an Armstrong number.";
+   echo "<br>************************************";
+
+
+// ------------------- asst-12 Display current time of the server -------------------------
+echo "<br>************************************";
+
+date_default_timezone_set('Asia/Kolkata');
+echo "<br><br>Current Time: ". date("h:i:sa");
 
 echo "<br>************************************";
+
+// ------------------- asst-13 Show good morning if it's time of morning -------------------------
+// Write a php program that display a different message based on time of day. For example page should display "Good morning" if it is accessed in the morning.
+echo "<br>************************************";
+
+date_default_timezone_set("Asia/Kolkata"); // Set your timezone here
+
+$currentHour = date("H");
+
+if ($currentHour >= 5 && $currentHour < 12) {
+    echo "<h3>Good morning!</h3>";
+} elseif ($currentHour >= 12 && $currentHour < 18) {
+    echo "<h3>Good afternoon!</h3>";
+} elseif ($currentHour >= 18 && $currentHour < 22) {
+    echo "<h3>Good evening!</h3>";
+} else {
+    echo "<h3>Good night!</h3>";
+}
+
+echo "<br>************************************";
+
+// ------------------- asst-14 Count vowels and consonent from a string -------------------------
+echo "<br>************************************";
+
+$string = $_POST['user_string'];
+$vowels = array("a", "e", "i", "o", "u");
+$consonents = array("b","c","d","f","g","h","j","k","l","m","n","p","r","s","t","v","w","x","y","z");
+$vowelCount = 0;
+$consonentCount = 0;
+
+for($i = 0 ; $i < strlen($string); $i++){
+	if(in_array($string[$i], $vowels))
+        $vowelCount++;
+  else if (in_array($string[$i], $consonents))
+        $consonentCount++;
+}
+         
+echo '<br/>Vowels : '. $vowelCount .'<br/>Consonants : '. $consonentCount;
+echo "<br>************************************";
+
+// ------------------- asst-15 Calculate the Electricity Bill in PHP -------------------------
 
 
 
